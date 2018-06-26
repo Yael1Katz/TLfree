9d0de4a625e016a947f69658372d73c6d3336262
Yael1Katz <39523778+Yael1Katz@users.noreply.github.com>
2018-06-26 15:43:01 +0300

    1

--- js/common-methods.js
+++ js/common-methods.js
@@ -3,7 +3,7 @@ function createEventLocationList() {
     eventsLocation["-LFpfOWugzcXfer8NHSv"] = { address: "אלנבי 99, Tel Aviv-Yafo, il" };
     eventsLocation["-LFCDwyGEtHoZH_VwgLZ"] = { address: "Rothschild Blvd 22, Tel Aviv-Yafo" };
     eventsLocation["-LFGeDD5_1LlFgr3Fs3o"] = { address: "hangar 23 namal tlv" };
-    eventsLocation["-LFGdu0__jw5eCZwUWkc"] = { address: "אחד העם 54" };
+    eventsLocation["-LFGdu0__jw5eCZwUWkc"] = { address: "אחד העם 54, תל אביב יפו" };
     eventsLocation["-LFkkDOXZnxUDm8BoDyl"] = { address: "54 Ahad Ha'Am Street" };
 
 

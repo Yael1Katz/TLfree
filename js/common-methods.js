a8d4a10e46ee2b45624a174933a94b529be6c9df
Yael1Katz <39523778+Yael1Katz@users.noreply.github.com>
2018-06-26 17:56:34 +0300

    1

--- js/common-methods.js
+++ js/common-methods.js
@@ -1,9 +1,9 @@
 function createEventLocationList() {
-    /*eventsLocation["-LFpf_y7DttCe0dkJxvE"] = { address: "Derech Menachem Begin 121, Tel Aviv-Yafo" };
+    eventsLocation["-LFpf_y7DttCe0dkJxvE"] = { address: "Derech Menachem Begin 121, Tel Aviv-Yafo" };
     eventsLocation["-LFpfOWugzcXfer8NHSv"] = { address: "אלנבי 99, Tel Aviv-Yafo, il" };
     eventsLocation["-LFCDwyGEtHoZH_VwgLZ"] = { address: "Rothschild Blvd 22, Tel Aviv-Yafo" };
     eventsLocation["-LFGdu0__jw5eCZwUWkc"] = { address: "אחד העם 54, תל אביב יפו" };
-    eventsLocation["-LFkkDOXZnxUDm8BoDyl"] = { address: "54 Ahad HaAm Street" };*/
+    eventsLocation["-LFkkDOXZnxUDm8BoDyl"] = { address: "54 Ahad HaAm Street" };
     eventsLocation["-LFGeDD5_1LlFgr3Fs3o"] = { address: "hangar 23 namal tlv" };
     
 

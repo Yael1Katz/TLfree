db4e03c3372ea4d788f0029ed2fcb2296a37c2cd
Yael1Katz <39523778+Yael1Katz@users.noreply.github.com>
2018-06-27 11:12:17 +0300

    1

--- js/common-methods.js
+++ js/common-methods.js
@@ -5,9 +5,19 @@ function createEventLocationList() {
     eventsLocation["-LFGdu0__jw5eCZwUWkc"] = { address: "אחד העם 54, תל אביב יפו" };
     eventsLocation["-LFkkDOXZnxUDm8BoDyl"] = { address: "54 Ahad HaAm Street" };
     eventsLocation["-LFGeDD5_1LlFgr3Fs3o"] = { address: "hangar 23 namal tlv" };
+    eventsLocation["-LFCDibfpJ8th5d_RBUr"] = { address: "2 Tushiya Street Tel Aviv-Yafo" };
+    eventsLocation["-LFCDFJSwKPqzcAZMDJK"] = { address: "121 Menachem Begin 121, Tel Aviv, Israel" };
+    eventsLocation["-LFCCmOjN63WgeQ8TSXS"] = { address: "הירקון 165, תל אביב יפו" };
+    eventsLocation["-LFkju_tF2Z-llZPz5fQ"] = { address: "39 Rothschild Tel Aviv-Yafo" };
+    eventsLocation["-LFarXkn6Ecq9KrHxs9V"] = { address: "Hamanor 14  Tel Aviv , Jaffa" };
+    eventsLocation["-LExkMzDpPAUpTn1iMhg"] = { address: "Urban Place - Rothschild" };
+    eventsLocation["-LExk5ZakxXPtVG50I7w"] = { address: "Iban Gabirol 30, Tel Aviv-Yafo" };
+    eventsLocation["-LFMXfGXDtCHSYwn4ien"] = { address: "שלמה אבן גבירול 69, תל אביב יפו" };
+    eventsLocation["-LFkjc_cAV8Eyk-chmgP"] = { address: "David Elazar 12, Tel Aviv-Yafo" };
+    eventsLocation["-LExjrl2Zt13XcPLe7iJ"] = { address: "Ahad Haam 54, Tel Aviv, Floor 5, Tel Aviv-Yafo, il" };
+    eventsLocation["-LFpfOWugzcXfer8NHSv"] = { address: "טיילור מייד, אלנבי, תל אביב יפו" };
+
     
-
-
 
 
 

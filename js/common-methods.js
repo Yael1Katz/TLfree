84018bc597ea7e11e806e9e4ee1bd5d8d6d91c01
Yael1Katz <39523778+Yael1Katz@users.noreply.github.com>
2018-06-30 16:51:29 +0300

    1

--- js/common-methods.js
+++ js/common-methods.js
@@ -1,9 +1,11 @@
 function createEventLocationList() {
+    eventsLocation = {};
+
     eventsLocation["-LFpf_y7DttCe0dkJxvE"] = { address: "Derech Menachem Begin 121, Tel Aviv-Yafo" };
     eventsLocation["-LFCDwyGEtHoZH_VwgLZ"] = { address: "Rothschild Blvd 22, Tel Aviv-Yafo" };
     eventsLocation["-LFGdu0__jw5eCZwUWkc"] = { address: "אחד העם 54, תל אביב יפו" };
     eventsLocation["-LFkkDOXZnxUDm8BoDyl"] = { address: "54 Ahad HaAm Street" };
-    eventsLocation["-LFGeDD5_1LlFgr3Fs3o"] = { address: "hangar 23 namal tlv" };
+    eventsLocation["-LFGeDD5_1LlFgr3Fs3o"] = { address: "namal tlv" };
     eventsLocation["-LFCDibfpJ8th5d_RBUr"] = { address: "2 Tushiya Street Tel Aviv-Yafo" };
     eventsLocation["-LFCDFJSwKPqzcAZMDJK"] = { address: "121 Menachem Begin 121, Tel Aviv, Israel" };
     eventsLocation["-LFCCmOjN63WgeQ8TSXS"] = { address: "הירקון 165, תל אביב יפו" };
@@ -54,6 +56,7 @@ function createEventLocationList() {
     eventsLocation["-LEsTX5ALg4PumiV4RWK"] = { address: "Azrieli Sarona Tower. 53rd floor, Tel Aviv, il" };
     eventsLocation["-LFkjLa5y_Zu-8GQ3QDr"] = { address: "Yigal Alon Street 98, Tel Aviv-Yafo" };
     eventsLocation["-LFCD0r5d32WWiaIWbC2"] = { address: "אחוזת בית 2, תל אביב יפו" };
-    
+
     //eventsLocation[""] = { address: "" };
+    return eventsLocation;
 }
